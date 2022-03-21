d5fe45264db7aee2608ceed0e5c4b2da5a840f17
Freefaf <sumkh287@gmail.com>
2022-03-21 14:10:18 +0530

    something

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+function diceroll(){
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
@@ -19,11 +19,11 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  document.querySelector("h1").innerHTML = " Player Red Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  document.querySelector("h1").innerHTML = "Player Blue Wins! ";
 }
 else {
   document.querySelector("h1").innerHTML = "Draw!";
-}
+}}
